6f2018dadf14ee8b2c79016b5e789e7b2730efb6
Stephen Sabey <stephen.sabey@gov.ab.ca>
2022-03-21 17:47:30 -0600

    publich update

--- UtilityFunctions/Function1.cs
+++ UtilityFunctions/Function1.cs
@@ -9,11 +9,25 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Net.Http;
 using System.Net;
+using System.Web;
+using System.Net.Http.Json;
 
 namespace UtilityFunctions
 {
     public static class Function1
     {
+        public class MadiResult
+        {
+            public bool statusOk { get; set; }
+            public string exception { get; set; }
+            public bool result { get; set; }
+            public string retMessage { get; set; }
+        }
+        public static HttpClient MadiClient { get; } = new() { BaseAddress = new Uri("https://account.alberta.ca/") };
+        public static string MadiEmailQuery(string email) => $"sa/doesemailexist?mail={email}";
+        public static HttpClient MadiBClient { get; } = new() { BaseAddress = new Uri("https://business.account.alberta.ca/") };
+        public static string MadiBEmailQuery(string email) => $"sawrapper/doesemailexist?mail={email}&loginType=business";
+
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
@@ -21,21 +35,36 @@ namespace UtilityFunctions
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string email = req.Query["email"];
+            string emailEncoded = HttpUtility.UrlEncode(email ?? "");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            MadiResult madiResult = new MadiResult();
+            MadiResult madiBResult = new MadiResult();
+            if (!string.IsNullOrEmpty(emailEncoded))
+            {
+                try
+                {
+                    var madiResponse = await MadiClient.GetAsync(MadiEmailQuery(emailEncoded));
+                    madiResult = await madiResponse.Content.ReadFromJsonAsync<MadiResult>();
+                    madiResult.statusOk = madiResponse.IsSuccessStatusCode;
+                }
+                catch (Exception ex)
+                {
+                    madiResult = new MadiResult { exception = ex.Message };
+                }
+                try
+                {
+                    var madiBResponse = await MadiBClient.GetAsync(MadiBEmailQuery(emailEncoded));
+                    madiBResult = await madiBResponse.Content.ReadFromJsonAsync<MadiResult>() ?? new MadiResult();
+                    madiBResult.statusOk = madiBResponse.IsSuccessStatusCode;
+                }
+                catch (Exception ex)
+                {
+                    madiBResult = new MadiResult { exception = ex.Message };
+                }
+            }
 
-            var response = new HttpResponseMessage(HttpStatusCode.OK);
-            response.Content = new StringContent($"<html><body>Hello World ({DateTimeOffset.Now:U})</body></html>", null, "text/html");
-            return (IActionResult)response;
-
-            //string responseMessage = string.IsNullOrEmpty(name)
-            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            //return new OkObjectResult(responseMessage);
+            return new OkObjectResult(new { email, madiResult, madiBResult });
         }
     }
 }
